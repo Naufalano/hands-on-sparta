26390d4c5cb065d4166778c4accb54489796b3b7
Naufalano <19623078@std.stei.itb.ac.id>
2024-08-10 06:12:00 +0700

    feat: new lines of HTML, updated profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,61 @@
 import Image from "next/image";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="bg-cyan-950">
+      <div className="bg-sky-300 items-center flex flex-col border-b-8 border-slate-950 border-double">
+        <h1 className="text-4xl font-serif p-5 bg-slate-300 text-black border-x-8 border-slate-950">PORTOFOLIO DIRI</h1>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className="flex flex-col items-center pt-10">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        src={"/Picture 002.png"}
+        alt="Foto Diri"
+        title="Naufal/Ano"
+        width={200}
+        height={300}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+        <div className="flex flex-col justify-items-center text-justify gap-2 p-5 max-w-2xl">
+          <h1 className="mainHeader ">Nama</h1>
+          <p className="text-center text-2xl ">Muhammad Naufal  Fathan</p>
+          <br></br>
+          <h1 className="mainHeader ">About Me</h1>
+          <p className="indent-8 ">
+            Biasa dipanggil Naufal atau Ano. Berasal dari Tangerang Selatan dan tinggal di Cisitu Indah V selama berkuliah 
+            di ITB Kampus Ganesha. Sekarang adalah seorang mahasiswa di jurusan Sistem dan Teknologi Informasi. Lahir 2 Maret 
+            2004 dan merupakan anak pertama dari 3 bersaudara. 
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <br></br>
+          <h1 className="mainHeader ">Minat & Bakat</h1>
+          <p className="indent-8 ">
+            Saat ini saya berminat untuk menekuni bidang data dan <i>cyber-security</i>. Namun, bakat yang relevan untuk 
+            bidang informatika belum terlihat. Bakat yang dirasa dimiliki bukanlah hal yang luar biasa, saya rasa hal ini 
+            cukup lumrah dan wajar, seperti cepat menghafal dan cepat paham. Meski merasa memiliki bakat demikian, saya 
+            juga memiliki kekurangan yang sangat berkebalikan, yaitu cepat lupa.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+          <br></br>
+          <h1 className="mainHeader ">Proyek & Pengalaman</h1>
+          <p className="indent-8 ">
+            Saya belum pernah terlibat atau mengerjakan proyek apapun. Namun, apabila dapat disebutkan, saya ikut membuat 
+            proyek game <i>Pokemon-like</i> sebagai tugas besar dasar pemrograman di TPB. Untuk pengalaman kepanitiaan, saya 
+            pernah menjadi ketua divisi Qur'an dalam kepanitiaan Ramadhan 1445H. Sewaktu MA, saya menjadi bagian dari OSIS 
+            Seksi Bahasa sebagai staf. Saya juga pernah menjadi panitia perlombaan pidato bahasa Inggris di MA. 
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <br></br>
+          <h1 className="mainHeader ">Pencapaian</h1>
+          <p className="">
+            - Menuntaskan setoran hafalan Qur'an 30 juz dan lulus ujian 20 juz.
           </p>
-        </a>
+          <br />
+          <h1 className="mainHeader ">Kontak Diri</h1>
+          <p className="">
+            Silakan hubungi saya di : <br />
+            - Email : <a href="mailto:fathan.naufal.m@gmail.com"  target="_blank" className="text-blue-300 hover:text-blue-500 hover:underline active:text-violet-300">fathan.naufal.m@gmail.com</a> <br />
+            - LinkedIn : <a href="https://www.linkedin.com/in/naufal-fathan-85b450289" target="_blank" className="text-blue-300 hover:text-blue-500 hover:underline active:text-violet-300 visited:text-violet-400">linkedin.com/in/naufal-fathan-85b450289</a>
+          </p>
+        </div>
       </div>
     </main>
   );
 }
+
+export default Home;
